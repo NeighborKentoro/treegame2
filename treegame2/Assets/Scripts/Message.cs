e8abc277a2d08082abac891dc101869a9ec1d624
nickstrelic <nstrelic@gmail.com>
2024-01-27 13:11:45 -0600

    prob broken

--- treegame2/Assets/Scripts/Message.cs
+++ treegame2/Assets/Scripts/Message.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Message : MonoBehaviour
 {
@@ -21,5 +22,37 @@ public class Message : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void setMessage(string message)
+    {
+        this.message = message;
+        var children = this.gameObject.GetComponentsInChildren<Transform>();
+        foreach (var child in children)
+        {
+            if (child.gameObject.name == "Name")
+            {
+                child.gameObject.GetComponent<TMP_Text>().text = playerColor == Color.green ? "Green" : "Hmm";
+                child.gameObject.GetComponent<TMP_Text>().color = playerColor;
+            }
+            if (child.gameObject.name == "Message")
+            {
+                child.gameObject.GetComponent<TMP_Text>().text = message;
+
+                this.gameObject.transform.localScale = new Vector2(this.gameObject.transform.localScale.x, child.gameObject.transform.localScale.y);
+            }
+        }
+    } 
+
+    public void setPlayerColor(Color playerColor)
+    {
+        this.playerColor = playerColor;
+        if (this.playerColor == GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().getColor())
+        {
+            //this.gameObject.transform.position = new Vector2(-189, this.transform.position.y);
+        } else
+        {
+            //this.gameObject.transform.position = new Vector2(0, this.transform.position.y);
+        }
     }
 }
